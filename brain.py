d1cc36375d2788523ddab0bcc8daa5378e79d1ce
Lukas_Franz <122890837+kggzfdtuf@users.noreply.github.com>
2025-11-21 14:42:53 +0100

    Update brain.py

--- brain.py
+++ brain.py
@@ -5,14 +5,19 @@ class NeuralNetwork():
         if old_brain is None:
             for i in range(8):
                 for j in range(5):
-                    self.weights["input-hidden"][i][j] = random.random()
+                    for k in range(2):
+                        self.weights["input-hidden"][i][j][k] = random.random()
             for i in range(5):
-                self.weights["hidden-output"][i][0] = random.random()
+                for k in range(2):
+                    self.weights["hidden-output"][i][0][k] = random.random()
             for i in range(8):
-                self.biases["input"][i] = random.random(-1, 1)
+                for k in range(2):
+                    self.biases["input"][i][k] = random.random(-1, 1)
             for i in range(5):
-                self.biases["hidden"][i] = random.random(-1, 1)
-            self.biases["output"][0] = random.random(-1, 1)
+                for k in range(2):
+                    self.biases["hidden"][i][k] = random.random(-1, 1)
+            for k in range(2):
+                self.biases["output"][0][k] = random.random(-1, 1)
         else:
             self = old_brain.mutate()
                     #inputs = [vel, acc, raytracing 6 times]
@@ -20,7 +25,7 @@ class NeuralNetwork():
         for j in range(len(self.weights["input-hidden"][i])):
             for i in range(len(inputs)):
                 for k in range(2):
-                    x[k] += self.weights["input-hidden"][i][j][0] * (inputs[i][0] + self.biases["input][i][0])
+                    x[k] += self.weights["input-hidden"][i][j][0] * (inputs[i][0] + self.biases["input"][i][0])
             hidden_inputs[j] += (x[0], x[1])
         for j in range(len(self.weights["hidden-output"][i])):
             for i in range(len(hidden_inputs)):
@@ -31,3 +36,21 @@ class NeuralNetwork():
             for k in range(2):
                 x[k] += self.weights["hidden-output"][i][0][0] * (hidden_inputs[i][0] + self.biases["hidden"][i][0])
         output += (x[0], x[1])
+    def mutate(self):
+        for i in self.weights:
+            for j in self.weights[i]
+                for k in self.weights[i][j]:
+                    for l in self.weights[i][j][k]:
+                        self.weights[i][j][k][l] + random.random(-1, 1)
+                        if self.weights[i][j][k][l] >= 1:
+                            self.weights[i][j][k][l] = 1
+                        if self.weights[i][j][k][l] <= -1:
+                            self.weights[i][j][k][l] = -1
+        for i in self.biases:
+            for j in self.biases[i]:
+                for k in self.biases[i][j]:
+                    self.biases[i][j][k] + random.random(-1, 1)
+                        if self.biases[i][j][k] >= 1:
+                            self.biases[i][j][k] = 1
+                        if self.biases[i][j][k] <= -1:
+                            self.biases[i][j][k] = -1
