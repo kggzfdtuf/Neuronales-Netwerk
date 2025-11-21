21dba28313c5d86f8573c7834b7794c2cf667e95
Lukas_Franz <122890837+kggzfdtuf@users.noreply.github.com>
2025-11-21 15:02:32 +0100

    Fix syntax errors and improve mutation logic

--- brain.py
+++ brain.py
@@ -22,26 +22,30 @@ class NeuralNetwork():
             self = old_brain.mutate()
                     #inputs = [vel, acc, raytracing 6 times]
     def compute(self, inputs):
+        hidden_inputs = []
+        output_input = []
+        output = []
+        x = [0, 0]
         for j in range(len(self.weights["input-hidden"][i])):
             for i in range(len(inputs)):
                 for k in range(2):
-                    x[k] += self.weights["input-hidden"][i][j][0] * (inputs[i][0] + self.biases["input"][i][0])
+                    x[k] += self.weights["input-hidden"][i][j][k] * (inputs[i][k] + self.biases["input"][i][k])
             hidden_inputs[j] += (x[0], x[1])
         for j in range(len(self.weights["hidden-output"][i])):
             for i in range(len(hidden_inputs)):
                 for k in range(2):
-                    x[k] += self.weights["hidden-output"][i][j][0] * (hidden_inputs[i][0] + self.biases["hidden"][i][0])
+                    x[k] += self.weights["hidden-output"][i][j][k] * (hidden_inputs[i][k] + self.biases["hidden"][i][k])
             output_input[j] += (x[0], x[1])
-        for i in range(len(ouput_inputs)):
+        for i in range(len(output_input)):
             for k in range(2):
-                x[k] += self.weights["hidden-output"][i][0][0] * (hidden_inputs[i][0] + self.biases["hidden"][i][0])
+                x[k] += self.weights["hidden-output"][i][0][k] * (hidden_inputs[i][k] + self.biases["hidden"][i][k])
         output += (x[0], x[1])
     def mutate(self):
         for i in self.weights:
-            for j in self.weights[i]
+            for j in self.weights[i]:
                 for k in self.weights[i][j]:
                     for l in self.weights[i][j][k]:
-                        self.weights[i][j][k][l] + random.random(-1, 1)
+                        self.weights[i][j][k][l] += random.random(-1, 1)
                         if self.weights[i][j][k][l] >= 1:
                             self.weights[i][j][k][l] = 1
                         if self.weights[i][j][k][l] <= -1:
@@ -49,8 +53,8 @@ class NeuralNetwork():
         for i in self.biases:
             for j in self.biases[i]:
                 for k in self.biases[i][j]:
-                    self.biases[i][j][k] + random.random(-1, 1)
-                        if self.biases[i][j][k] >= 1:
-                            self.biases[i][j][k] = 1
-                        if self.biases[i][j][k] <= -1:
-                            self.biases[i][j][k] = -1
+                    self.biases[i][j][k] += random.random(-1, 1)
+                    if self.biases[i][j][k] >= 1:
+                        self.biases[i][j][k] = 1
+                    if self.biases[i][j][k] <= -1:
+                        self.biases[i][j][k] = -1
